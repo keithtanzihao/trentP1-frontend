0f2f180725c5d104117d88ad4f24f917946bac74
Keith Tan <keithtanzihao@gmail.com>
2022-02-19 23:52:22 +0800

    map.js: Display map, Load airbnb data, Categorised by room_type

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,20 +1,54 @@
+// Obtain access_token later
+let access_token = "";
 let attributionText =
-  'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>';
+  '<img src="https://docs.onemap.gov.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>';
 
-let access_token =
-  "pk.eyJ1Ijoia2VpdGh0aGV0YW4iLCJhIjoiY2t6aHM4d3YyMGZhajJvcGExMHpuY3IyeCJ9.Fu1vfDSJW62iLCKg5a-WVg";
+// Reference from https://www.onemap.gov.sg/docs/maps/
+let center = L.bounds([1.56073, 104.11475], [1.16, 103.502]).getCenter();
+let map = L.map("map").setView([center.x, center.y], 13);
 
-let L = require('leaflet');
-let singapore = [1.3521, 103.8198];
-let map = L.map('map').setView(singapore, 11.5);
-
-let renderedMap = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+let basemap = L.tileLayer(
+  "https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png",
   {
-    attribution: attributionText,
-    id: "mapbox/streets-v11",
-    accessToken: access_token,
+    detectRetina: true,
+    maxZoom: 18,
+    minZoom: 12,
+    preferCanvas: true,
   }
-).addTo(map);
+);
 
+map.setMaxBounds([
+  [1.56073, 104.1147],
+  [1.16, 103.502],
+]);
+basemap.addTo(map);
 
+function jsonColumnBreakDown(property, data, storeObj, colorArray) {
+  let propertyType = data[property];
+  if (!(data[property] in storeObj)) {
+    storeObj[data[property]] = colorArray.pop();
+  }
+  return storeObj[data[property]];
+}
+
+window.addEventListener("DOMContentLoaded", async function () {
+  let response = await axios.get("../data/listings.json");
+
+  // Need to refactor in future
+  let colorArray = ["#F51720", "#FC642D", "#00A699", "#767676"];
+  let checkRooms = {};
+
+  for (let t of response.data) {
+    let lat = parseFloat(t["latitude"]);
+    let lng = parseFloat(t["longitude"]);
+
+    tColor = jsonColumnBreakDown("room_type", t, checkRooms, colorArray);
+
+    let options = {
+      radius: 10,
+      color: tColor,
+    };
+    L.circle(L.latLng(lat, lng), options).addTo(map);
+  }
+  console.log(checkRooms);
+});
