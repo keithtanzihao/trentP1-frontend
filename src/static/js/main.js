e26c8192e70f71f44b1ccd682e23257fbd604b63
Keith Tan <keithtanzihao@gmail.com>
2022-02-24 16:05:46 +0800

    main.js: Made changes to modal closing button & included active/hidden classNames for modal div

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -4,26 +4,17 @@ let navInput = document.querySelector("#nav-input");
 navInput.addEventListener("change", (event) => {
   console.log("testing");
   if (event.target.checked) {
-    document.querySelector(".modal").classList.remove("hidden");
+    document.querySelector("#modal").classList.remove("hidden");
   } else {
-    document.querySelector(".modal").classList.add("hidden");
+    document.querySelector("#modal").classList.add("hidden");
   }
 });
 
 // Allows user to open modal if button click
-// let modalBox = document.querySelector("#modal-cb");
-// console.log(modalBox);
+let modalBox = document.querySelector(".modal-btn");
+console.log(modalBox);
 
-// modalBox.addEventListener("click", (event) => {
-//   // if (event.target.checked) {
-//   //   console.log("yes");
-//   //   document.querySelector("#modal").classList.remove("active");
-//   //   document.querySelector("#modal").classList.add("hidden");
-//   // }
-//   // console.log("yes");
-//   alert("testing button here");
-//   document.querySelector("#modal").classList.remove("active");
-//   document.querySelector("#modal").classList.add("hidden");
-// });
-
-console.log(navInput);
+modalBox.addEventListener("click", (event) => {
+  document.querySelector("#modal").classList.remove("active");
+  document.querySelector("#modal").classList.add("hidden");
+});
