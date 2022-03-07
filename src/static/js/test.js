1b7d862451d46ba0caa3f6611db4ab56b1e46219
Keith Tan <keithtanzihao@gmail.com>
2022-03-07 21:37:16 +0800

    Deleted: Removed all deleted files

--- src/static/js/test.js
+++
@@ -1,8 +0,0 @@
-console.log("testing");
-
-window.addEventListener("DOMContentLoaded", function () {
-  let modalBox = document.getElementById("modal-box-x");
-  modalBox.addEventListener("click", function () {
-    alert("testing2");
-  });
-});
